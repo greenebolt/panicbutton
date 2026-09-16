5062bd0bc0ff2545bd55032b482ac91f1dbc478a
GreeneBolt <thegreenebold@gmail.com>
2026-09-16 19:25:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Panic Button</h1>
+
+<img width="1225" height="126" alt="PanicButtonBanner" src="https://github.com/user-attachments/assets/6b9304e7-7db2-42e9-ae27-47391ac83f08" />
+
+<p align="center">A handy dandy lightweight mod to aid in the difficult process to leave, save and quit, disconnect, exit, etc.</p>
+
+Features:
+
+⚜ Configurable keybind that quits the game
+
+⚜ Works in both Singleplayer & Multiplayer
+
+⚜ Works in version 1.21.11
---
+++ src/main/java/greenebolt/panicbutton/PanicButton.java
@@ -0,0 +1,58 @@
+package greenebolt.panicbutton;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PanicButton implements ModInitializer {
+	public static final String MOD_ID = "panicbutton";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		KeyMapping.Category CATEGORY = KeyMapping.Category.register(Identifier.parse("panicbutton"));
+		String KEY = "key.panicbutton.quit_game";
+
+		KeyMapping panickey;
+		panickey = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+				KEY,
+				InputConstants.Type.KEYBOARD,
+				188,
+				CATEGORY
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (panickey.consumeClick()) {
+				Disconnect();
+			}
+		});
+
+		LOGGER.info("Panic Button Initialized!");
+	}
+
+	public static void Disconnect() {
+		Minecraft mc = Minecraft.getInstance();
+		if (mc.getSingleplayerServer() != null) {
+			mc.execute(() -> {
+				mc.disconnectFromWorld(Component.translatable("Panic Quit!"));
+			});
+			return;
+		}
+		if (mc.player == null) return;
+		mc.execute(() -> {
+			mc.disconnect(new TitleScreen(), false);
+		});
+	}
+}
